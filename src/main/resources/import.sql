24f0312867bd0c8b8e00701e1b5d7f278fd881eb
luan-coelho <luancoelho.dev@gmail.com>
2022-11-21 18:02:42 -0300

    Import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_user (id, name, email, password)
+VALUES (nextval('hibernate_sequence'), 'Luan', 'luan@gmail.com', '123');
+
+INSERT INTO tb_user (id, name, email, password)
+VALUES (nextval('hibernate_sequence'), 'Ana', 'ana@gmail.com', '123');
+
+INSERT INTO tb_user (id, name, email, password)
+VALUES (nextval('hibernate_sequence'), 'Maria', 'maria@gmail.com', '123');
+
+INSERT INTO tb_user (id, name, email, password)
+VALUES (nextval('hibernate_sequence'), 'Pedro', 'pedro@gmail.com', '123');
